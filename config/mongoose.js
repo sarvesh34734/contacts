b90b87e4b4e0cff7db8b515e4950e1f71e1851be
sarvesh34734 <42905079+sarvesh34734@users.noreply.github.com>
2020-06-02 01:32:48 +0530

    updated monoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,8 +3,9 @@ const mongoose=require("mongoose");
 
 //connect to he database
 
-mongoose.connect("mongodb+srv://sarveshvyas:sudo%23linux%40202@matrix-xbssh.mongodb.net/test?retryWrites=true&w=majority",{useNewUrlParser: true});
-
+//connect to the database
+const url= process.env.MONGODB_URI || "mongodb://localhost/contacts_list_db";
+mongoose.connect(url);
 //acquire the connection(to check if it is successfull)
 const db=mongoose.connection;
 
